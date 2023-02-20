58fe2bd104bf655f03d03294e575696b2a71a944
wnagleiofficial <798306069@qq.com>
2023-02-20 15:38:29 +0800

    fix pretrain script

--- ProtFlash/pretrain.py
+++ ProtFlash/pretrain.py
@@ -4,14 +4,14 @@ import torch
 from .model import FLASHTransformer
 from .utils import load_hub_workaround
 
-MODEL_URL = "https://zenodo.org/record/7104813/files/flash_protein.pt"
+MODEL_URL_BASE = "https://zenodo.org/record/7655858/files/protflash_large.pt"
 
 
 def load_prot_flash_base():
-    model_data = load_hub_workaround(MODEL_URL)
+    model_data = load_hub_workaround(MODEL_URL_BASE)
     # model_data = torch.load("/mnt/d/protein-net/ProtBert/flash_protein.pt", map_location="cpu")
     hyper_parameter = model_data["hyper_parameters"]
-    model = FLASHTransformer(hyper_parameter['dim'], hyper_parameter['num_tokens'], hyper_parameter['num_layers'], group_size=hyper_parameter['num_tokens'],
+    model = FLASHTransformer(hyper_parameter['dim'], hyper_parameter['num_tokens'], hyper_parameter['num_layer'], group_size=hyper_parameter['num_tokens'],
                              query_key_dim=hyper_parameter['qk_dim'], max_rel_dist=hyper_parameter['max_rel_dist'], expansion_factor=hyper_parameter['expansion_factor'])
 
     model.load_state_dict(model_data['state_dict'])
