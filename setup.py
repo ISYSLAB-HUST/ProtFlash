9814013bb15a8d007472294363f7e57d40812b8f
wnagleiofficial <798306069@qq.com>
2023-07-03 21:32:39 +0800

    fix version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='ProtFlash',
-      version='0.1.0',
+      version='0.1.1',
       description='protein language model',
       long_description=long_description,
       long_description_content_type="text/markdown",
